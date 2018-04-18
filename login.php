6e38d2b9e26b7e63cbe774d49aa93ff2c73690f9
longdij <longptse03552@fpt.edu.vn>
2018-04-19 01:26:10 +0700

    login
    menu

--- login.php
+++ login.php
@@ -4,52 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="template/image/icons/favicon.ico"/>
+    <link rel="icon" type="image/png" href="./image/icons/favicon.ico"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="./fonts/iconic/css/material-design-iconic-font.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="./vendor/animate/animate.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="./vendor/css-hamburgers/hamburgers.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="./vendor/animsition/css/animsition.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="./vendor/select2/select2.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="./vendor/daterangepicker/daterangepicker.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="template/css/util.css">
-    <link rel="stylesheet" type="text/css" href="template/css/main.css">
+    <link rel="stylesheet" type="text/css" href="./css/util.css">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
     <!--===============================================================================================-->
-    <link href="template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template -->
+    <!-- Custom fonts for this . -->
     <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="template/css/one-page-wonder.min.css" rel="stylesheet">
+    <!-- Custom styles for this . -->
+    <link href="./css/one-page-wonder.min.css" rel="stylesheet">
 </head>
 <body>
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="template/image/Logo-FU-01.png"></a>
+        <a class="navbar-brand" href="#"><img src="./image/Logo-FU-01.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Sign Up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Log In</a>
-                </li>
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" href="#">Sign Up</a>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" href="#">Log In</a>-->
+<!--                </li>-->
             </ul>
         </div>
     </div>
@@ -57,10 +57,10 @@
 
 <form action="index.php?action=login&controller=account" method="post">
     <div class="limiter">
-        <div class="container-login100" style="background-image: url('template/image/bg-01.jpg');">
+        <div class="container-login100" style="background-image: url('./image/bg-01.jpg');">
             <div class="wrap-login100">
-                <form class="login100-form validate-form" style="background-image: url('template/image/logo.png');">
-					<span class="login100-form-logo" style="background-image: url('template/image/logo.png');">
+                <form class="login100-form validate-form" style="background-image: url('./image/logo.png');">
+					<span class="login100-form-logo" style="background-image: url('./image/logo.png');">
 						<i class="zmdi zmdi-landscape"></i>
 					</span>
 
@@ -107,24 +107,24 @@
     </footer>
     <div id="dropDownSelect1"></div>
     <!--===============================================================================================-->
-    <script src="template/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="./vendor/jquery/jquery-3.2.1.min.js"></script>
     <!--===============================================================================================-->
-    <script src="template/vendor/animsition/js/animsition.min.js"></script>
+    <script src="./vendor/animsition/js/animsition.min.js"></script>
     <!--===============================================================================================-->
-    <script src="template/vendor/bootstrap/js/popper.js"></script>
-    <script src="template/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="./vendor/bootstrap/js/popper.js"></script>
+    <script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
     <!--===============================================================================================-->
-    <script src="template/vendor/select2/select2.min.js"></script>
+    <script src="./vendor/select2/select2.min.js"></script>
     <!--===============================================================================================-->
-    <script src="template/vendor/daterangepicker/moment.min.js"></script>
-    <script src="template/vendor/daterangepicker/daterangepicker.js"></script>
+    <script src="./vendor/daterangepicker/moment.min.js"></script>
+    <script src="./vendor/daterangepicker/daterangepicker.js"></script>
     <!--===============================================================================================-->
-    <script src="template/vendor/countdowntime/countdowntime.js"></script>
+    <script src="./vendor/countdowntime/countdowntime.js"></script>
     <!--===============================================================================================-->
-    <script src="template/js/main.js"></script>
+    <script src="./js/main.js"></script>
     <!-- Bootstrap core JavaScript -->
-    <script src="template/vendor/jquery/jquery.min.js"></script>
-    <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./vendor/jquery/jquery.min.js"></script>
+    <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </form>
 </body>
